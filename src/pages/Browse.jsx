14443470a434ad5e8034e9d8ce54ec46c79a98a2
james <ivanlavan773@gmail.com>
2025-07-24 09:30:17 +0300

    Deleted pages

--- src/pages/Browse.jsx
+++
@@ -1,8 +0,0 @@
-function browse() {
-  return (
-    <div className="page-content">
-     <h2>Browse book here!</h2>
-    </div>
-  );
-}
-export default browse;
--- src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-function Home() {
-  return (
-    <div className="page-content">
-      <h2>Welcome to BookBuddy!</h2>
-      <p>Discover and share your favorite books.</p>
-    </div>
-  );
-}
-export default Home;
