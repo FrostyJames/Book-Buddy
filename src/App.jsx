5a7856ad68c66b089d201113c9aff6a1e0e0792b
Angel Wambui <angel.wambui@student.moringaschool.com>
2025-07-24 09:18:25 +0300

    updated code for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import Browse from './pages/Browse';
+import Submit from './pages/Library';
+import './App.css'; 
+
 
 function App() {
- 
-
+  
   return (
     <>
-     
+    <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/browse" element={<Browse />} />
+        <Route path="/submit" element={<Submit />} />
+      </Routes>
+
     </>
   )
 }
